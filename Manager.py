5a201ab75899da318dd6d172655e65b141704ca7
Rafael Caldeira <rafael.caldeira.2005@gmail.com>
2024-05-23 19:11:19 -0300

    refactor: ProblemManager class moved to Problems file

--- Manager.py
+++ Manager.py
@@ -2,8 +2,15 @@ from abc import ABC, abstractmethod
 import sqlite3
 
 class Manager(ABC):
-    def __init__(self, nome_banco):
-        self.conexao = sqlite3.connect(nome_banco)
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = object().__new__(cls)
+        return cls.__instance
+
+    def __init__(self, database: str):
+        self.conexao = sqlite3.connect(database)
         self.cursor = self.conexao.cursor()
 
     @abstractmethod
@@ -25,28 +32,5 @@ class Manager(ABC):
     def close_connection(self):
         self.conexao.close()
 
-class GerenciadorCategorias(Manager):
-    def register(self, descricao, sla):
-        self.cursor.execute("INSERT INTO CategoriasProblema (Descricao, SLA) VALUES (?, ?)", (descricao, sla))
-        self.conexao.commit()
 
-    def view(self):
-        self.cursor.execute("SELECT * FROM CategoriasProblema")
-        return self.cursor.fetchall()
-
-    def update(self, id_categoria, nova_descricao, novo_sla):
-        self.cursor.execute("UPDATE CategoriasProblema SET Descricao = ?, SLA = ? WHERE ID = ?", (nova_descricao, novo_sla, id_categoria))
-        self.conexao.commit()
-
-    def delete(self, id_categoria):
-        self.cursor.execute("DELETE FROM CategoriasProblema WHERE ID = ?", (id_categoria,))
-        self.conexao.commit()
-
-    def associate_sla(self, id_categoria, tempo_sla):
-        self.cursor.execute("INSERT INTO SLA (CategoriaID, Tempo) VALUES (?, ?)", (id_categoria, tempo_sla))
-        self.conexao.commit()
-
-    def get_sla(self, id_categoria):
-        self.cursor.execute("SELECT Tempo FROM SLA WHERE CategoriaID = ?", (id_categoria,))
-        return self.cursor.fetchone()
 
