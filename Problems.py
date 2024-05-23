051b483b576cc7f2f7bb93dca115a836bef9e6b7
ViniciusFS1 <fioravante.vinicius@gmail.com>
2024-05-23 17:15:21 -0300

    Update Problems.py

--- Problems.py
+++ Problems.py
@@ -13,16 +13,16 @@ class ProblemsManager(Manager):
         self.DAO = ProblemsDBDAO() #define a classe ProblemsDBDAO como DAO
     
     def register(self, problem: Problem):   #chama a função dao para se conectar com o banco de dados dos problemas e registrar um prblema
-        self.DAO.insert(problem.id,problem.description,problem.sla)
+        self.DAO.insert([problem.id,problem.description,problem.sla])
         
     def view(self, problem: Problem):
         pass
     
     def update(self, problem: Problem):   #chama a função dao para se conectar com o banco de dados dos problemas e atualizar um problema
-        self.DAO.update(problem.id, problem.description, problem.sla)
+        self.DAO.update([problem.id, problem.description, problem.sla])
 
     def delete(self, problem: Problem):  #chama a função dao para se conectar com o banco de dados dos problemas e deletar um problema
-        self.DAO.delete(problem.id)
+        self.DAO.delete([problem.id])
     
     def priority(self, problem: Problem, sla : int):
         pass
