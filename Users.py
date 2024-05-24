7e1b113af55cf3a45f3569fedc72c1589ee41d7d
Carol10418747 <167645179+Carol10418747@users.noreply.github.com>
2024-05-23 22:05:12 -0300

    fix: now UsersManager works with database

--- Users.py
+++ Users.py
@@ -1,29 +1,35 @@
 from Database import *
 from Manager import Manager
-from Person import Person
+from dataclasses import dataclass
 
-class Users(Person):
-    def __init__(self, password: str, position: str) -> None:
-        super().__init__(id, name, email)
-        self.password = password
-        self.position = position
+@dataclass
+class Users():
+    id: int
+    name: str
+    email: str
+    password: str
+    position: str
 
 
 class UsersManager(Manager):
     def __init__(self):
         self.DAO = UsersDBDAO()
     
-    def register(self, user: Users):
-        self.DAO.insert(user.id, user.name, user.email, user.password, user.position)
+    def register(self, user: Users) -> None:
+        self.DAO.insert([user.id, user.name, user.email, user.password, user.position])
         
-    def view(self):
-        pass
+    def view(self, userID: int) -> None:
+        data = self.DAO.read(userID)
+        print(f"ID: {data['id']}\n\
+                Name: {data['name']}\n\
+                Email: {data['email']}\n\
+                Position: {data['position']}")
     
-    def update(self):
-        pass
+    def update(self, userID: int, updateData: dict) -> None:
+        self.DAO.update(userID, updateData)
 
-    def delete(self):
-        pass
+    def delete(self, userID: int) -> None:
+        self.DAO.delete(userID)
 
 '''
 As funções precisam:
