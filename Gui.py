3b6201b503c075b34cc1dcb7fea3ce0973e61bae
Riccio <riccio.fernando@gmail.com>
2024-05-23 21:30:23 -0300

    Add 2 New Main Window Types and Move assets

--- Gui.py
+++ Gui.py
@@ -2,10 +2,11 @@ from tkinter import *
 from tkinter import messagebox
 from customtkinter import *
 from PIL import Image
+from abc import ABC, abstractmethod
 
-class MainWindow:
+class MainWindow(ABC):
     '''Janela principal do sistema de gestão'''
-    def __init__(self, parent) -> None:
+    def __init__(self, parent, user) -> None:
         #Criar Janela Principal
         self.window = Toplevel(parent)
         self.window.title("Menu - Sistema Integrado de Gestão Empresarial")
@@ -14,13 +15,54 @@ class MainWindow:
         self.height = 450
         self.window.geometry(f"{self.width}x{self.height}")
 
+    @abstractmethod
+    def createWidgets(self):
+        '''Cria as entradas e botões da janela, retorna o próprio objeto'''
+        return self
+
+class MainWindowSpecial(MainWindow):
+    ''''Janela para Atendentes/Técnicos'''
+    def createWidgets():
+        pass
+    
+    # Essa janela deve conter as opções de:
+    #     Adição/Remoção/Visualização/Alteração de:
+    #         Usuários
+    #         Clientes
+    #         Categorias de problemas
+    #         Chamados
+    #     Listar Chamados Baseado em diferentes parametros:  
+    #         Status
+    #         Abertura
+    #         Fechamento
+    #         Máxima para atendimento
+
+
+
+class MainWindowRegular(MainWindow):
+    '''Janela para usuários normais'''
+    def createWidgets():
+        pass
+
+    # Essa janela só deve conter as opções de:
+        # criar chamados
+        # visualizar chamados
+        # (Obviamente somente os chamados próprios)
+
+class MainWindowSelector():
+    def createWindow(self, user, parent):
+        #Verificar qual tipo de usuário é e criar janela adequada
+        if(True):
+            return MainWindowRegular(parent, user)
+        elif(True):
+            return MainWindowSpecial(parent, user)
 
 class StartScreen:
     '''Janela parente de todas as outras e também janela de login'''
     def __init__(self): 
         #Criando Janela Filha de login
         self.window = Tk("Login")
-        self.window.iconbitmap("Icon.ico")
+        self.window.iconbitmap("assets/Icon.ico")
         self.window.title("Log In -  Sistema Integrado de Gestão Empresarial")
         #Definindo altura e largura
         self.width = 300
@@ -28,10 +70,12 @@ class StartScreen:
         self.window.geometry(f"{self.width}x{self.height}")
         #Impedindo a modificação do tamanho da janela
         self.window.resizable(False, False)
+        #criação do seletor de janela
+        self.windowSelector = MainWindowSelector()
         self.__createComponents()
 
     def __createComponents(self): 
-        self.logoImage = CTkLabel(self.window, 200, 200, 5, text="", image=CTkImage(Image.open("LoginImage.png"), size=(120,120)))
+        self.logoImage = CTkLabel(self.window, 200, 200, 5, text="", image=CTkImage(Image.open("assets/LoginImage.png"), size=(120,120)))
         self.logoImage.place(relx=0.5, rely=0.25, anchor=CENTER)
         #Criando Entrada de Login
         self.loginField = CTkEntry(self.window, 200, 28, 5)                                        
@@ -52,7 +96,7 @@ class StartScreen:
         if(a == True): 
             #Ocultar janela de login e criar janela principal
             self.window.withdraw()
-            MainWindow(self.window)
+            self.windowSelector.createWindow("", self.window)
         else:
             #Criar mensagem de erro
             messagebox.showwarning('Inválido', "Usuário ou Senha Inválidos.")
@@ -61,5 +105,6 @@ class StartScreen:
         '''Inicializar janela de login'''
         self.window.mainloop()
 
-a = StartScreen()
-a.initialize()
+if __name__ == '__main__':
+    a = StartScreen()
+    a.initialize()
