e688eff3fe61f9e2d5d5a602f817a93292f2f018
Rafael Caldeira <rafael.caldeira.2005@gmail.com>
2024-05-28 18:15:30 -0300

    test: Problems and ProblemsManager tested

---
+++ test_Problems.py
@@ -0,0 +1,70 @@
+import pytest
+import os
+import sqlite3
+from Problems import *
+from dataclasses import asdict
+
+def silentRemove(filename: str) -> None:
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+
+databaseName = "problemsTest.db"
+
+@pytest.fixture(scope="module")
+def problemsManager():
+    silentRemove(databaseName)
+    problemManager = ProblemsManager(databaseName)
+    problemManager.register(Problems('Server down', 2))
+    yield problemManager
+    silentRemove(databaseName)
+
+def test_createProblem():
+    problem = Problems('Server down', 5)
+    data = asdict(problem)
+    assert data == {'description': 'Server down', 'sla': 5}
+
+def test_register(problemsManager):
+    problemsManager.register(Problems('Server down', 2))
+    conn = None
+    try:
+        conn = sqlite3.connect(databaseName)
+        cursor = conn.cursor()
+        cursor.execute('''SELECT * FROM Problem WHERE ID = 2''')
+        assert cursor.fetchone() == (2, 'Server down', '2')
+    except:
+        assert False
+    finally:
+        if conn:
+            conn.close()
+
+def test_update(problemsManager):
+    newData = {'description': 'License Expired', 'sla': 1}
+    problemsManager.update(1, newData)
+    conn = None
+    try:
+        conn = sqlite3.connect(databaseName)
+        cursor = conn.cursor()
+        cursor.execute('''SELECT * FROM Problem WHERE ID = 1''')
+        assert cursor.fetchone() == (1, 'License Expired', '1')
+    except:
+        assert False
+    finally:
+        if conn:
+            conn.close()
+
+def test_delete(problemsManager):
+    problemID = 1
+    problemsManager.delete(problemID)
+    conn = None
+    try:
+        conn = sqlite3.connect(databaseName)
+        cursor = conn.cursor()
+        cursor.execute('''SELECT * FROM Problem WHERE ID = 1''')
+        assert cursor.fetchone() is None
+    except:
+        assert False
+    finally:
+        if conn:
+            conn.close()
