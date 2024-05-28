47b8f8a53516c1dbf9c8509ac59a27650104fd0d
Rafael Caldeira <rafael.caldeira.2005@gmail.com>
2024-05-27 22:34:14 -0300

    test: UsersDBDAO unit tests implemented

---
+++ test_UsersDBDAO.py
@@ -0,0 +1,69 @@
+import pytest
+import os
+import sqlite3
+from Database import UsersDBDAO
+
+def silentRemove(filename: str) -> None:
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+
+databaseName = "usersTest.db"
+
+@pytest.fixture(scope="module")
+def usersDAO():
+    silentRemove(databaseName)
+    usersDAO = UsersDBDAO(databaseName)
+    userData = [1, 'User 1', 'user1@example.com', 'password123', 'Manager']
+    usersDAO.insert(userData)
+    yield usersDAO
+    silentRemove(databaseName)
+
+def test_create(usersDAO):
+    usersDAO.create()
+    connection = sqlite3.connect(databaseName)
+    cursor = connection.cursor()
+    cursor.execute("PRAGMA table_info('User')")
+    table_info = cursor.fetchall()
+    connection.close()
+    assert len(table_info) == 5
+    assert table_info[0][1] == 'id'
+    assert table_info[1][1] == 'name'
+    assert table_info[2][1] == 'email'
+    assert table_info[3][1] == 'password'
+    assert table_info[4][1] == 'position'
+
+def test_insert(usersDAO):
+    userData = [1, 'User 1', 'user1@example.com', 'password123', 'Manager']
+    usersDAO.insert(userData)
+    connection = sqlite3.connect(databaseName)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM User WHERE id = 1")
+    data = cursor.fetchone()
+    connection.close()
+    assert data == tuple(userData)
+
+def test_read(usersDAO):
+    userID = 1
+    userData = usersDAO.read(userID)
+    assert userData == {'id': 1, 'name': "User 1", 'email': "user1@example.com", 'password': "password123", 'position': "Manager"}
+
+def test_update(usersDAO):
+    userID = 1
+    newData = {'id': userID, 'name': 'User Updated', 'email': 'updated@example.com', 'password': 'newpassword', 'position': 'Employee'}
+    usersDAO.update(userID, newData)
+    userData = usersDAO.read(userID)
+    assert userData == newData
+
+def test_delete(usersDAO):
+    userID = 1
+    usersDAO.delete(userID)
+    data = usersDAO.read(userID)
+    assert data == {}
+
+def test_getUserByEmailAndPassword(usersDAO):
+    userEmail = 'user1@example.com'
+    userPassword = 'password123'
+    userData = usersDAO.getUserByEmailAndPassword(userEmail, userPassword)
+    assert userData == {'id': 1, 'name': "User 1", 'email': "user1@example.com", 'password': "password123", 'position': "Manager"}
